6a44a1e10ff24958c3baaa7c4da46b364b19fa05
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-09-15 16:40:25 -0400

    Revert "Deployement configuration changes"
    This reverts commit e4757fa9f0841759eddd33b912fa445774e34761.

--- html/wp-config.php
+++ html/wp-config.php
@@ -38,17 +38,16 @@ if (!function_exists('getenv_docker')) {
 		}
 	}
 }
-/* Make WordPress install plugins directly */
-define('FS_METHOD', 'direct');
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
 
 /** Database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'wordpress') );
+define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'wordpress') );
+define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
 
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
@@ -57,7 +56,7 @@ define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'wordpress') );
  */
 
 /** Database hostname */
-define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mariadb') );
+define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
